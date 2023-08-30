ed684be11bc2fbc82c135c198089c135d98fa6d5
沫烬染 <mojinran@outlook.com>
2023-08-30 19:13:01 +0800

    修改消息样式，添加hover效果

--- src/main/kotlin/FurryLoaderMC/TelegramBot/Utils.kt
+++ src/main/kotlin/FurryLoaderMC/TelegramBot/Utils.kt
@@ -1,6 +1,8 @@
 package FurryLoaderMC.TelegramBot
 
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.JoinConfiguration
+import net.kyori.adventure.text.event.HoverEvent
 import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer
 import net.kyori.adventure.text.format.NamedTextColor
 
@@ -8,10 +10,17 @@ import net.kyori.adventure.text.format.NamedTextColor
 object Utils {
 
     fun furryLoaderMessage(telegram: String, minecraft: String, message: String): Component {
-        val header = Component.text("[FurryLoader] ").color(NamedTextColor.GREEN)
-        val name = Component.text("<${telegram}>(${minecraft}) ").color(NamedTextColor.YELLOW)
-        val content = Component.text(message).color(NamedTextColor.WHITE)
-        return Component.text("").append(header).append(name).append(content)
+        return Component.join(
+            JoinConfiguration.separator(Component.text(" ")),
+            Component.text("[FurryLoader]").color(NamedTextColor.GREEN),
+            Component.join(
+                JoinConfiguration.separator(null),
+                Component.text("<"),
+                Component.text(telegram).hoverEvent(HoverEvent.showText(Component.text(minecraft))),
+                Component.text(">")
+            ).color(NamedTextColor.YELLOW),
+            Component.text(message)
+        )
     }
 
 
