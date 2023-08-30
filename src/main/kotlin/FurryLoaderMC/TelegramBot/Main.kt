6931b7cdd0131c5ba019c8c98a9793ec1613e290
沫烬染 <mojinran@outlook.com>
2023-08-30 15:56:46 +0800

    优化代码，分出发消息的逻辑

--- src/main/kotlin/FurryLoaderMC/TelegramBot/Main.kt
+++ src/main/kotlin/FurryLoaderMC/TelegramBot/Main.kt
@@ -7,14 +7,37 @@ import org.bukkit.plugin.java.JavaPlugin
 class Main : JavaPlugin() {
 
     companion object {
+
         lateinit var instance: JavaPlugin
         lateinit var webSocket: WebSocket
+
+
+        fun sendMessageToGame(telegram: String, minecraft: String, message: String) {
+            val content = Utils.furryLoaderMessage(telegram, minecraft, message)
+            this.instance.server.sendMessage(content)
+        }
+
+
+        fun sendMessageToBot(message: String) {
+            this.webSocket.sendMessage(message)
+        }
+
+
+        fun outputLoggerInfo(content: String) {
+            this.instance.logger.info(content)
+        }
+
+    }
+
+
+
+    override fun onLoad() {
+        instance = this
+        webSocket = WebSocket()
     }
 
 
     override fun onEnable() {
-        instance = this
-        webSocket = WebSocket()
         webSocket.start()
         Bukkit.getPluginManager().registerEvents(EventListener(), this)
     }
